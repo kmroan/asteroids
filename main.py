13c94bcda945cf37c8673f9ad73689edc6657fea
Keith Roan <11397454+kmroan@users.noreply.github.com>
2026-01-22 20:48:22 -0500

    ch3/l2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from logger import log_state
+from player import Player
 import pygame
 import constants
 
@@ -11,7 +12,7 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,6 +20,7 @@ def main():
                 return
             pass
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
