fc4ed1f2fc37b09df7bbc79a53d28641db5d21ee
Keith Roan <11397454+kmroan@users.noreply.github.com>
2026-01-22 19:07:47 -0500

    fin ch3

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,7 +20,7 @@ def main():
             pass
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
